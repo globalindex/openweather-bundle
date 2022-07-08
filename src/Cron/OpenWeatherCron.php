4eeb00a218fb17c561409465a4efcb5f4c67798e
Dietrich Bojko <webinteger@gmail.com>
2022-07-08 22:18:27 +0200

    Add Cron File

---
+++ src/Cron/OpenWeatherCron.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Webinteger\OpenWeatherBundle\Cron;
+
+class OpenWeatherCron
+{
+    public function onHourly(): void
+    {
+        # code...
+    }
+}
--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -5,3 +5,4 @@ declare(strict_types=1);
 use Webinteger\OpenWeatherBundle\Model\OpenWeatherModel;
 
 $GLOBALS['TL_MODELS']['tl_openweather'] = OpenWeatherModel::class;
+// $GLOBALS['TL_CRON']['hourly'][] = [OpenWeatherCron::class, 'onHourly'];
