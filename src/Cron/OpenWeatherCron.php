fbca1cbb73dbca71a11d553db34d4659eabe399f
Dietrich Bojko <webinteger@gmail.com>
2022-07-09 10:43:26 +0200

    Add Cron Update

--- src/Cron/OpenWeatherCron.php
+++ src/Cron/OpenWeatherCron.php
@@ -4,19 +4,28 @@ declare(strict_types=1);
 
 namespace Webinteger\OpenWeatherBundle\Cron;
 
+use Contao\Config;
 use Psr\Log\LoggerInterface;
 use Contao\CoreBundle\Framework\ContaoFramework;
 use Contao\CoreBundle\ServiceAnnotation\CronJob;
+use Doctrine\DBAL\Connection;
 
-/**
- * @CronJob("hourly")
- */
 class OpenWeatherCron
 {
-    public function __construct(ContaoFramework $framework, LoggerInterface $logger)
+    private ContaoFramework $framework;
+    private Connection $connection;
+    private LoggerInterface $logger;
+
+    public function __construct(ContaoFramework $framework, Connection $connection, LoggerInterface $logger)
     {
+        $this->framework = $framework;
+        $this->connection = $connection;
+        $this->logger = $logger;
     }
 
+    /**
+    * @CronJob("hourly")
+    */
     public function __invoke(): void
     {
         $this->logger->info('OpenWeather Cron funktioniert');
